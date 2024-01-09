7dc985df86995f1beb4d501ba82c65279882e783
TheTrueFax <130262457+TheTrueFax@users.noreply.github.com>
2024-01-10 10:37:15 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http= require("http");
+const fs= require("fs");
 
 const server=http.createServer((req,res)=>{
   console.log("server is visit");
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("hello");
+  fs.readFile("index.html",function(err,data){
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 });
 
 server.listen(3000,"0.0.0.0",function(){
