4780c4582109c7752d083fc6edbdc88e7977d8f8
TheTrueFax <130262457+TheTrueFax@users.noreply.github.com>
2024-01-11 13:00:21 +1300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const server=http.createServer((req,res)=>{
 */
   if (req.method=="POST") {
 
-console.log("body: "+req.body);
     console.log(req);
 
     res.writeHead(200,{'Content-Type':'text/plain'});
